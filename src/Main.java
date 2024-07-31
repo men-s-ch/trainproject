1f2fc7c3dfc9a1818ad584b66881be8f33b7c156
Jeongjinbeom <wlsqjal12345@naver.com>
2024-07-31 18:37:12 +0900

    Main.java 수정입니다

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!!!");
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			System.out.println("기차관리 프로그램 입니다");
+			break;
+		}
+	}
 }
